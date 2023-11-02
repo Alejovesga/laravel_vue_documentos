8a98d482adb54510be348cf1a0d98b5ece3300dd
alejovesga <alejovesga25@gmail.com>
2023-11-02 12:13:43 -0500

    Se añaden modelos de documentos, procesos, tipo y consecutivo con sus respectivas migraciones

---
+++ database/migrations/2023_11_02_164400_create_doc_documentos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doc_documentos', function (Blueprint $table) {
+            $table->id();
+            $table->string("DOC_NOMBRE");
+            $table->int("DOC_CODIGO");
+            $table->text("DOC_CONTENIDO");
+            $table->int("DOC_ID_TIPO");
+            $table->int("DOC_ID_PROCESO");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doc_documentos');
+    }
+};
---
+++ database/migrations/2023_11_02_164755_create_con_consecutivos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('con_consecutivos', function (Blueprint $table) {
+            $table->id();
+            $table->int("CON_CONSECUTIVO");
+            $table->string("CON_ID_PROCESO");
+            $table->string("CON_ID_TIPO");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('con_consecutivos');
+    }
+};
